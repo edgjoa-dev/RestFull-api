f7ffe49345ba8a19dfc137ed683207d9e384f48e
Edgar Joaquin <floresedgarjoaquin@gmail.com>
2025-12-01 14:32:50 -0600

    feature/129-filedValidationsCreateNewUser: Se crean las validaciones necesarias para la creación de nuevo usuario con express-validator

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,5 +1,7 @@
 import { Router } from 'express';
 import { usersGet, userGet, createUser, updateUser, deleteUser } from '../controllers/users.controller.js';
+import { check } from 'express-validator';
+import { fieldValidator } from '../middleware/index.js';
 
 const router = Router();
 
@@ -7,7 +9,13 @@ router.get('/', usersGet);
 
 router.get('/:id', userGet);
 
-router.post('/', createUser);
+router.post('/',[
+    check('name', 'El nombre de usuario es obligatorio').notEmpty(),
+    check('email', 'El email es obligatorio').isEmail().notEmpty(),
+    check('password', 'El password es obligatorio y de 9 caracteres minimo').isLength({ min: 9 }).notEmpty(),
+    check('role', 'El rol no es válido').isIn(['ADMIN_ROLE', 'USER_ROLE']),
+    fieldValidator
+],createUser);
 
 router.put('/:id', updateUser);
 
